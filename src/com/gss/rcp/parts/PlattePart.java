356ea87748fd86b6394b8408f686cb8f746bd80a
sunxiran <maricle01@163.com>
2017-07-05 13:46:40 +0800

    migration

---
+++ src/com/gss/rcp/parts/PlattePart.java
@@ -0,0 +1,24 @@
+package com.gss.rcp.parts;
+
+import javax.annotation.PostConstruct;
+
+import org.eclipse.gef.fx.swt.canvas.FXCanvasEx;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.widgets.Composite;
+
+import javafx.scene.Scene;
+import test2.example.gef4.mvc.logo.MvcLogoExample;
+
+public class PlattePart {
+
+	@PostConstruct
+	public void createContent(Composite parent) {
+		System.out.println("FxtestPart createContent!");
+		FXCanvasEx fxCanvasEx = new FXCanvasEx(parent, SWT.NONE);
+		fxCanvasEx.setScene(createScene());
+	}
+
+	private Scene createScene() {
+		return new MvcLogoExample().provideScene();
+	}
+}
