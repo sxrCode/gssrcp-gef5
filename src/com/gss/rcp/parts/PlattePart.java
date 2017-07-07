6cb95f4f9f42b6e31c87bbe951bd2e779626314d
sunxiran <maricle01@163.com>
2017-07-07 09:26:52 +0800

    又回到最初的起点

--- src/com/gss/rcp/parts/PlattePart.java
+++ src/com/gss/rcp/parts/PlattePart.java
@@ -1,10 +1,11 @@
 package com.gss.rcp.parts;
 
 import javax.annotation.PostConstruct;
+import javax.inject.Inject;
 
-import org.apache.log4j.Logger;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.widgets.Composite;
+import org.osgi.service.log.LogService;
 
 import com.gss.rcp.examples.gef5.mvc.logo.MvcLogoExample;
 
@@ -12,9 +13,13 @@ import javafx.embed.swt.FXCanvas;
 import javafx.scene.Scene;
 
 public class PlattePart {
+	
+	@Inject
+	private LogService logger;
 
 	@PostConstruct
 	public void createContent(Composite parent) {
+		logger.log(LogService.LOG_ERROR, "PlattePart createContent!");
 		FXCanvas canvas = new FXCanvas(parent, SWT.NONE);
 		canvas.setScene(getScene());
 	}
